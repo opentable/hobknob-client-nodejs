df611eb17ddb1481ef26cb7a72a4a9866df344c8
Chris Ridde <criddle@opentable.com>
2014-07-08 11:34:13 +0100

    Refactored client

--- src/Client.js
+++ src/Client.js
@@ -2,59 +2,66 @@ var Etcd = require("node-etcd"),
     util = require("util"),
     _ = require("underscore");
 
-function Client(applicationName, environment, etcdConfig) {
+var getToggleKey = function(applicationName, toggleName){
+    return util.format("v1/toggles/%s/%s", applicationName, toggleName);
+};
 
-    etcdConfig = _.defaults(etcdConfig || {},
+var validateResponse = function(response, err){
+    if (err){
+        return err;
+    }
+    // todo: will the response or node ever be null?
+    if (response == null || response.node === null || response.node.value === null){
+        return new Error("Key not found");
+    }
+};
+
+var getValue = function(etcdNode){
+    var value = etcdNode.value.toLowerCase();
+    switch(value)
+    {
+        case "true": return true;
+        case "false": return true;
+        default: return null;
+    }
+};
+
+var getOrDefault = function(etcd, applicationName, toggleName, defaultValue, callback){
+    var key = getToggleKey(applicationName, toggleName);
+    etcd.get(key, function(err, response){
+
+        var error = validateResponse(response.node, err);
+        if (error){
+            callback(error, defaultValue);
+            return;
+        }
+
+        var value = getValue(response.node);
+        if (value){
+            callback(null, value);
+        } else {
+            callback(new Error("Invalid value for toggle: " + toggleName), defaultValue);
+        }
+    });
+};
+
+function Client(applicationName, etcdConfig) {
+
+    var etcdConfig = _.defaults(etcdConfig || {},
         {
             host: "127.0.0.1",
             port: 4001
         });
     this.etcd = new Etcd(etcdConfig.host, etcdConfig.port);
     this.applicationName = applicationName;
-    this.environment = environment;
 }
 
-var getKey = function(applicationName, environment, toggleName){
-    return util.format("v1/toggles/%s/%s/%s", applicationName, environment, toggleName);
-};
-
 Client.prototype.get = function(toggleName, callback){
-    var key = getKey(this.applicationName, this.environment, toggleName);
-    this.etcd.get(key, function(err, value, headers){
-
-        // todo: clean this up and refactor, too ugly!
-        if (err){
-            callback(err, null);
-        } else if (value.node.value === null){
-            callback(null, null); // pass exception
-        } else if (value.node.value.toLowerCase() === "true"){
-            callback(null, true);
-        } else if (value.node.value.toLowerCase() === "false"){
-            callback(null, false);
-        } else {
-            callback(null, null); // pass exception
-        }
-
-    });
+    getOrDefault(this.etcd, this.applicationName, toggleName, null, callback);
 };
 
 Client.prototype.getOrDefault = function(toggleName, defaultValue, callback){
-    var key = getKey(this.applicationName, this.environment, toggleName);
-    this.etcd.get(key, function(err, value, headers){
-
-        // todo: clean this up and refactor, too ugly!
-        if (err){
-            callback(err, defaultValue);
-        } else if (value.node.value === null){
-            callback(null, null); // pass exception
-        } else if (value.node.value.toLowerCase() === "true"){
-            callback(null, true);
-        } else if (value.node.value.toLowerCase() === "false"){
-            callback(null, false);
-        } else {
-            callback(null, defaultValue); // pass exception
-        }
-    });
+    getOrDefault(this.etcd, this.applicationName, toggleName, defaultValue, callback);
 };
 
 module.exports = Client;
